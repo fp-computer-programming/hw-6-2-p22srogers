6dda62ce5b4cb6c876f0bd9237ba3204a58351a5
P22srogers <p22srogers@fairfieldprep.org>
2021-11-14 11:33:46 -0500

    Commit

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: SMR (AMDG) 11/9/21
+
+# Question 1
+lst=[0,1,2,3,4,5,6,7]
+
+# Question 2
+print(len(lst))
+
+# Question 3
+print(lst[2])
+
+# Question 4
+print(5 not in lst)
+
+# Question 5
+print(10 not in lst)
+
+# Question 6
+lst2 = ["James", "Luis", "Ryan", "Sean"]
+
+# Question 7
+print(lst2[3])
+
+# Question 8
+print(lst2 * 3)
+
+# Question 9
+print(lst + lst2)
+
+# Question 10
+print(len(lst2))
